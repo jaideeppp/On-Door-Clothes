d881e67bcaa95cc6de70b36acecf8301723833ac
jaideep <jaideepphalswal12@gmail.com>
2020-12-01 18:37:24 +0530

    shop page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/ShopPage'
 
 import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
